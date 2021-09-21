d19f5ca0597b0c516b8233481cafc1a4f523cd2d
Axel Rock <rockaxel@gmail.com>
2021-09-21 14:13:36 +0200

    Added inverted mode and shadow to Friends title generator

--- friends/main.js
+++ friends/main.js
@@ -1,35 +1,26 @@
-const dots = ['#fd0006', '#00b4ec', '#ffba00']
+const font = new FontFaceObserver('Friends'),
+  space =
+    String.fromCharCode(8201) + '<span>.</span>' + String.fromCharCode(8201)
 
-let canvas, currentText
+input.onchange = () => (output.innerHTML = input.value.split('').join(space))
 
-// const context = canvas.getContext('2d')
-// context.font = '100px Friends'
-// context.textAlign = 'center'
-// context.textBaseline = 'middle'
-
-const space =
-  String.fromCharCode(8201) + '<span>.</span>' + String.fromCharCode(8201)
-
-input.onchange = () => draw(input.value)
-
-function draw(text) {
-  currentText = text
-  output.innerHTML = text.split('').join(space)
-
+button.onclick = () => {
   html2canvas(output, {
     backgroundColor: null,
-  }).then(c => {
-    canvas = c
+  }).then(canvas => {
+    var link = document.createElement('a')
+    link.download = `friends-${input.value}.png`
+    link.href = canvas.toDataURL()
+    link.click()
   })
 }
 
-button.onclick = () => {
-  var link = document.createElement('a')
-  link.download = `friends-${currentText}.png`
-  link.href = canvas.toDataURL()
-  link.click()
+invert.onchange = () => {
+  document.body.classList.toggle('inverted')
 }
 
-var font = new FontFaceObserver('Friends')
+shadow.onchange = () => {
+  output.classList.toggle('shadow')
+}
 
-font.load().then(() => draw('friends'))
+font.load().then(() => (output.innerHTML = input.value.split('').join(space)))
