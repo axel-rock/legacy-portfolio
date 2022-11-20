7b7e407d031acbcf7602fba14bfff9676c6c8a87
Axel Rock <axel@bannerboy.com>
2022-11-20 22:28:00 +0000

    Add audio/scroll synchro on seats-io application

--- hi-seats-io/js/main.js
+++ hi-seats-io/js/main.js
@@ -6,3 +6,62 @@ document.querySelectorAll('.save-preference').forEach((element) => {
 		localStorage.setItem(element.id || element.name, element.value)
 	})
 })
+
+const highlightLink = document.querySelector('#attaboy-link')
+
+const voiceOver = document.querySelector('audio')
+
+const timeline = gsap
+	.timeline({ paused: true })
+	.to(document.body, { duration: 1, scrollTo: { y: '#summary', autokill: true } }, 9)
+	.to(
+		highlightLink,
+		{ duration: 5, backgroundPosition: highlightLink.getBoundingClientRect().width + 'px 0', ease: 'sine.inOut' },
+		57
+	)
+	.to(document.body, { duration: 1, scrollTo: { y: '#skills', autokill: true } }, 71.5)
+	.call(
+		() => {
+			document.querySelector('#js').open = false
+			document.querySelector('#node').open = true
+		},
+		null,
+		87
+	)
+	.call(
+		() => {
+			document.querySelector('#node').open = false
+			document.querySelector('#photoshop').open = true
+		},
+		null,
+		106
+	)
+	.call(
+		() => {
+			document.querySelector('#photoshop').open = false
+			document.querySelector('#observers').open = true
+		},
+		null,
+		120
+	)
+	.call(
+		() => {
+			document.querySelector('#observers').open = false
+			document.querySelector('#service-worker').open = true
+		},
+		null,
+		136
+	)
+	.to(document.body, { duration: 1, scrollTo: { y: '#why-seats-dot-io', autokill: true } }, 171)
+	.to(document.body, { duration: 1, scrollTo: { y: '#contact', autokill: true } }, 248)
+
+function onUpdate() {
+	timeline.time(voiceOver.currentTime)
+}
+
+voiceOver.onplay = function () {
+	gsap.ticker.add(onUpdate)
+}
+voiceOver.onpause = function () {
+	gsap.ticker.remove(onUpdate)
+}
